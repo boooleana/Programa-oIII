0106fe3c7f881c5d1221dfd1bac7d40532c84671
boooleana <giuliadenise10@gmail.com>
2018-03-19 00:16:06 -0300

    organizando arq

---
+++ Recapitulação/aula03.py
@@ -0,0 +1,13 @@
+pontos = [[1,2], [5,3], [3,5], [2,4]]
+distancia = 0
+soma = 0
+i = 0
+
+for i in range(0,len(pontos)):
+    if i+1 == len(pontos):
+        n = 0
+    else:
+        n = (i+1)
+    distancia = ((abs(pontos[i][0]-pontos[n][0]))**2 + (abs(pontos[i][1]-pontos[n][1]))**2)**(1/2)
+    soma += distancia
+print(soma)
